57f5cd10181ea3c86cb901309fdc550414c935db
Nikhil M Warrier <nikhilmwarrier@disroot.org>
2024-10-13 19:15:02 +0530

    Make LatestHeadlines dynamically styleable

--- src/components/LatestHeadlines.tsx
+++ src/components/LatestHeadlines.tsx
@@ -1,13 +1,13 @@
 import { Article } from "../types";
 
-const LatestHeadlines = ({ articles }: { articles: Article[] }) => {
+const LatestHeadlines = ({ articles, className }: { articles: Article[], className?: string }) => {
   return (
-    <div className="w-full m-auto p-3 border-solid border-2 border-gray-500">
+    <div className={`${className ? className : ''} w-full my-auto p-3 pl-6 border-solid border-l-2  border-gray-200`}>
       <h2 className="text-3xl mb-3">Latest headlines</h2>
       <ul role="list">
         {articles.map(article => (
           <li className="text-xl my-3 list-inside">
-            <a className="link">{article.title}</a>
+            <a href={article.url} className="link">{article.title}</a>
           </li>
         ))}
       </ul>
