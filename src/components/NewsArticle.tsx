7821f2c39da8c6f3544c0c1d1747d0e464e26f3c
Nikhil M Warrier <nikhilmwarrier@disroot.org>
2024-10-13 21:09:47 +0530

    Conditionally display metadata

--- src/components/NewsArticle.tsx
+++ src/components/NewsArticle.tsx
@@ -15,18 +15,22 @@ const NewsArticle = ({
         <h3 className={isHero ? "text-5xl" : "text-2xl"}>{article.title}</h3>
       </a>
       <div className="my-3 text-gray-800 text-sm">
-        Published at {new Date(article.publishedAt).toLocaleString()} <br />
-        Authored by {article.author} <br />
+        {article.publishedAt && <>Published at </>}
+        {new Date(article.publishedAt).toLocaleString()} <br />
+        {article.author && <>Authored by {article.author} </>}
+        <br />
       </div>
       <img
         className="my-3 mx-auto"
         src={article.urlToImage}
         alt={article.description}
       />
-      <p>{article.description}</p>
-      <a className="link" href={article.url}>
-        Source - {article.source.name}
-      </a>
+      {article.description && <p>{article.description}</p>}
+      {article.source.name && (
+        <a className="link" href={article.url}>
+          Source - {article.source.name}
+        </a>
+      )}
     </div>
   );
 };
