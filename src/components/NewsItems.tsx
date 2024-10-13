45d85d0dc1f32c04b92ea719e783061c966cd01a
Nikhil M Warrier <nikhilmwarrier@disroot.org>
2024-10-13 20:44:55 +0530

    Add error boundary

--- src/components/NewsItems.tsx
+++ src/components/NewsItems.tsx
@@ -9,43 +9,50 @@ const NewsItems = () => {
   const [articles, setArticles] = useState<Article[]>([]);
   const [loading, setLoading] = useState(true);
   const [category, setCategory] = useState<NewsCategory>("general");
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     setLoading(true);
     const getHeadlines = async () => {
-      const response = await fetch(
-        `https://newsapi.org/v2/top-headlines?country=us&pageSize=23&category=${category}`,
-        {
-          headers: {
-            Authorization: import.meta.env.VITE_NEWSAPI_KEY,
-          },
-        }
-      );
-      const data = (await response.json()) as TopHeadlines;
-      console.log(data.articles);
+      try {
+        const response = await fetch(
+          `https://newsapi.org/v2/top-headlines?country=us&pageSize=23&category=${category}`,
+          {
+            headers: {
+              Authorization: import.meta.env.VITE_NEWSAPI_KEY,
+            },
+          }
+        );
+        const data = (await response.json()) as TopHeadlines;
+        console.log(data.articles);
 
-      /* Only show articles that are not [Removed] */
-      const filteredArticles = data.articles.filter(
-        article => article.title !== "[Removed]"
-      );
+        /* Only show articles that are not [Removed] */
+        const filteredArticles = data.articles.filter(
+          article => article.title !== "[Removed]"
+        );
 
-      /* Look for the first article with an image and set it as the hero */
-      let heroArticleIndex = filteredArticles.findIndex(
-        article => article.urlToImage
-      );
-      let heroArticle = filteredArticles[heroArticleIndex];
-      filteredArticles.splice(heroArticleIndex, 1);
+        /* Look for the first article with an image and set it as the hero */
+        let heroArticleIndex = filteredArticles.findIndex(
+          article => article.urlToImage
+        );
+        let heroArticle = filteredArticles[heroArticleIndex];
+        filteredArticles.splice(heroArticleIndex, 1);
 
-      // The bottom grid looks ugly if the number of articles is NOT a multiple of `bottomGridSize`.
-      const bottomGridSize = 4;
+        // The bottom grid looks ugly if the number of articles is NOT a multiple of `bottomGridSize`.
+        const bottomGridSize = 4;
 
-      // Because of this, we remove excess elements
-      // Magic number: 8 = 1 (Hero) + 2 (Sub) + 5 (latestHeadlines)
-      const excess = (filteredArticles.length - 8) % bottomGridSize;
-      filteredArticles.splice(filteredArticles.length - 1 - excess, excess);
+        // Because of this, we remove excess elements
+        // Magic number: 8 = 1 (Hero) + 2 (Sub) + 5 (latestHeadlines)
+        const excess = (filteredArticles.length - 8) % bottomGridSize;
+        filteredArticles.splice(filteredArticles.length - 1 - excess, excess);
 
-      setArticles([heroArticle, ...filteredArticles]);
-      setLoading(false);
+        setArticles([heroArticle, ...filteredArticles]);
+      } catch (error) {
+        console.log(`Error: ${error}`);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
     };
 
     getHeadlines();
@@ -57,6 +64,8 @@ const NewsItems = () => {
 
       {loading ? (
         <h3 className="font-6xl text-center">Loading...</h3>
+      ) : error ? (
+        <p>An error occured :-/</p>
       ) : (
         <>
           <div className="grid lg:grid-cols-5 pb-6 gap-6 mb-12 border-b-2 border-gray-200">
