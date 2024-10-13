9f8b4bc2a7d787208dc621fb8d56ec54a4ab2f62
Nikhil M Warrier <nikhilmwarrier@disroot.org>
2024-10-13 19:14:11 +0530

    Minor styling changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="mt-12 mb-6">
+    <header className="mt-12 mb-12">
         <h1 className="text-5xl font-bold text-center text-gray-700">NewsApp</h1>
     </header>
   )
